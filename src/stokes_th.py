827329b4c2818767f0ce5e781d97a6ac947dd4de
elepiersan <eleonora@simula.no>
2016-06-17 13:51:16 +0200

    Changed stokes_th to a newer version

--- src/stokes_th.py
+++ src/stokes_th.py
@@ -1,19 +1,16 @@
 from dolfin import *
-mesh = UnitSquareMesh(32, 32)
-# P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-# P2 = FiniteElement("Lagrange", mesh.ufl_cell(), 2)
-# TH = (P2 * P2) * P1
-# 
-# W = FunctionSpace(mesh, TH)
+mesh = UnitSquareMesh(16, 16)
+P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+P2 = FiniteElement("Lagrange", mesh.ufl_cell(), 2)
+TH = (P2 * P2) * P1
 
-V = VectorFunctionSpace(mesh, "Lagrange", 2)  # space for velocity
-Q = FunctionSpace(mesh, "Lagrange", 1)        # space for pressure
-W = V * Q
+W = FunctionSpace(mesh, TH)
 
 # define solution
 x = SpatialCoordinate(mesh)
 y = (1-cos(0.8 * pi * x[0])) * (1-x[0])**2 \
   * (1-cos(0.8 * pi * x[1])) * (1-x[1])**2
+U0, U1 = -y.dx(1), y.dx(0)
 U = as_vector((-y.dx(1), y.dx(0))) # velocity
 F = - 2 * div(sym(grad(U)))        # forcing term
 G = - div(grad(U)) - F             # pressure gradient
@@ -25,7 +22,7 @@ v, q = TestFunctions(W)
 a = (inner(grad(u),grad(v))+ p * div(v) + q * div(u)) * dx
 L = inner(F, v) * dx
 
-bcs = [DirichletBC(W.sub(0), U, "on_boundary")]
+bcs = [DirichletBC(W.sub(0), (0., 0.), "on_boundary")]
 
 # solve
 A, b = assemble_system(a, L, bcs, A_tensor = PETScMatrix())
@@ -33,9 +30,9 @@ wh = Function(W); uh, ph = wh.split()
 solve(A, wh.vector(), b)
 
 # compute errors
-L2_error_U = assemble((U-uh)**2 * dx)
-H1_error_U = assemble(grad(U-uh)**2 * dx)
-H1_error_P = assemble((grad(ph) - G)**2 * dx) 
+L2_error_U = assemble((U-uh)**2 * dx)**.5
+H1_error_U = assemble(grad(U-uh)**2 * dx)**.5
+H1_error_P = assemble((grad(ph) - G)**2 * dx)**.5 
 
 print "||u - uh; L^2|| = {0:1.4e}".format(L2_error_U)
 print "||u - uh; H^1|| = {0:1.4e}".format(H1_error_U)
