5de281c1bb85b2756faca2bb9b45940396cd44ef
carlocis <carlocis@simula.no>
2016-06-28 10:40:30 +0200

    modified stokes

--- src/stokes.py
+++ src/stokes.py
@@ -4,7 +4,7 @@
 # div( u ) = 0
 
 from numpy import *
-from math import log 
+from math import *
 from matplotlib import pyplot as plt
 from fenics import *
 from mshr import *   # I need this if I want to use the functions below to create a mesh
@@ -16,8 +16,8 @@ N = [2**2, 2**3, 2**4, 2**5, 2**6]
 h = [1./i for i in N]
 h2 = [1./(i**2) for i in N]
 errsL2 = []
-errH1 = []
-errL2pressure = []
+errsH1 = []
+errsL2pressure = []
 rates1 = []
 rates2 = []
 rates3 = []
@@ -45,8 +45,8 @@ for n in N:
     
     # I have to remember that the u_exact has to satisfy as well the boundary conditions (and not only the system of equations)
     # that's why there's the pi*x[0], so the sin is 0 on the right boundary (i.e. x[0] = 1))
-    # u_exact = as_vector((0, sin(pi*x[0]))) # to use as a solution to verify the convergence 
-    u_exact = as_vector((0, x[0]*(1-x[0])))   # as_vector() ???
+    u_exact = as_vector((0, sin(pi*x[0]))) # to use as a solution to verify the convergence 
+    # u_exact = as_vector((0, x[0]*(1-x[0])))   # as_vector() ???
     p_exact = 0.5 - x[1]
     
     f = - nu*div(grad(u_exact)) + grad(p_exact)   # I changed the sign in the gradient
@@ -54,8 +54,8 @@ for n in N:
     # Since the pressure is defined up to some constant, we compare the gradients
     g =  nu*div(grad(u_exact)) + f             # pressure gradient
     
-    u_exact_e = Expression((" 0 ", "x[0]*(1-x[0])" ), domain=mesh, degree=2)
-    # u_exact_e = Expression((" 0 ", "sin(pi*x[0])" ))
+    # u_exact_e = Expression((" 0 ", "x[0]*(1-x[0])" ), domain=mesh, degree=2)
+    u_exact_e = Expression((" 0 ", "sin(pi*x[0])" ))
     p_exact_e = Expression("0.5-x[1]", domain=mesh, degree=1)
     
     # plot(u_exact_e, mesh = mesh, title = "exact velocity")
@@ -150,12 +150,9 @@ for n in N:
     L2_error_p = assemble((p_exact - ph)**2 * dx)**.5
     # H1_error_p = assemble((grad(ph) - g)**2 * dx)**.5
    
-   
-        
-    
     errsL2.append(L2_error_u)
-    errH1.append(H1_error_u)
-    errL2pressure.append(L2_error_p)
+    errsH1.append(H1_error_u)
+    errsL2pressure.append(L2_error_p)
     
     print "||u - uh; L^2|| = {0:1.4e}".format(L2_error_u)
     print "|u - uh; H^1| = {0:1.4e}".format(H1_error_u)
@@ -163,15 +160,16 @@ for n in N:
     #print "||p - ph; H^1|| = {0:1.4e}".format(H1_error_p)
     
     
-#for i in range(len(h)-1):
-    # rates1.append( log2(errsL2[i+1]/errsL2[i])/log2(h[i+1]/h[i]) )
+print errsL2
+for i in range(len(h)-1):
+    rates1.append(math.log(errsL2[i+1]/errsL2[i])/math.log(h[i+1]/h[i]) )
 
-#print rates1
+print rates1
 #print range(len(h)-1)
 
-# errH1 and h^2 are parallel hence the convergence rate is 0
+# errsH1 and h^2 are parallel hence the convergence rate is 2
 
-plt.loglog(h, errH1, label = 'Error H1 norm')
+plt.loglog(h, errsH1, label = 'Error H1 norm')
 plt.loglog(h, h2, label = 'h^2')
 plt.loglog(h,h, label = 'h')
 plt.xlabel('h')
@@ -189,8 +187,8 @@ plt.grid(True)
 
 plt.legend(loc = 'best')
 plt.show()
-#plt.savefig("convergence_sine.png")
-plt.savefig("convergence_poly.png")
+plt.savefig("convergence_sine.png")
+#plt.savefig("convergence_poly.png")
 
 
 # in order to see whether the convergence is quadratic, I have to
