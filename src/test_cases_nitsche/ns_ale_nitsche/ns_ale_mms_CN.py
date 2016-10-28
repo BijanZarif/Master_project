3244cbf036fb65c478a07672956319852c6c49f6
elepiersan <eleonora@simula.no>
2016-10-28 16:37:33 +0200

    test case ns_ale_mms_CN seems to work with fixed mesh and theta = 1.0

--- src/test_cases_nitsche/ns_ale_nitsche/ns_ale_mms_CN.py
+++ src/test_cases_nitsche/ns_ale_nitsche/ns_ale_mms_CN.py
@@ -10,37 +10,40 @@ DT = [1./N[i] for i in range(len(N))]
 
 rho = 1.0
 mu = 1.0/8.0
-theta = 0.5
-t_ = 0.0
-t0 = Constant(0.0)
-t1 = Constant(1.0)
+theta = 1.0
 C = 0.1
 
 def sigma(u,p):
-    return mu*grad(u) - p*Identity(2)
+    return 2.0*mu*sym(grad(u)) - p*Identity(2)
 
 for dt in DT:
     print "dt = {}".format(dt)
     for n in N:
         
+        t_ = 0.0
+        t0 = Constant(0.0)
+        t1 = Constant(dt)
+        
         print "n = {}".format(n)
+
         mesh = UnitSquareMesh(n, n)
         x = SpatialCoordinate(mesh)
         normal = FacetNormal(mesh)
         
         ##EP: Why do you use Expression? It is better to use directly UFL
         
-        u_exact_e = Expression(("sin(2*pi*x[1])*cos(2*pi*x[0])*cos(t)", "-sin(2*pi*x[0])*cos(2*pi*x[1])*cos(t)"), t = t_)
-        w_exact_e = Expression(("C*sin(2*pi*x[1])*cos(t)", "0.0"), C=C, t = t_)
-        # w_exact_e = Expression(("0.0", "0.0"))
-        p_exact_e = Expression("cos(x[0])*cos(x[1])*cos(t)", t = t_)
+        u_exact_e = Expression(("sin(2*pi*x[1])*cos(2*pi*x[0])*cos(t)", "-sin(2*pi*x[0])*cos(2*pi*x[1])*cos(t)"), t = t1)
+        # w_exact_e = Expression(("C*sin(2*pi*x[1])*cos(t)", "0.0"), C=C, t = t1)
+        w_exact_e = Expression(("0.0", "0.0"))
+        p_exact_e = Expression("cos(x[0])*cos(x[1])*cos(t)", t = t1)
         
         #Write exact solution using UFL
         u_exact0 = as_vector(( sin(2*pi*x[1])*cos(2*pi*x[0])*cos(t0) , -sin(2*pi*x[0])*cos(2*pi*x[1])*cos(t0) ))
         u_exact1 = as_vector(( sin(2*pi*x[1])*cos(2*pi*x[0])*cos(t1) , -sin(2*pi*x[0])*cos(2*pi*x[1])*cos(t1) ))
-        w_exact0 = as_vector(( C*sin(2*pi*x[1])*cos(t0) , 0.0))
-        w_exact1 = as_vector(( C*sin(2*pi*x[1])*cos(t1) , 0.0))
-        # w_exact = as_vector((0.0, 0.0))
+        # w_exact0 = as_vector(( C*sin(2*pi*x[1])*cos(t0) , 0.0))
+        # w_exact1 = as_vector(( C*sin(2*pi*x[1])*cos(t1) , 0.0))
+        w_exact0 = Constant((0.0,0.0))
+        w_exact1 = Constant((0.0,0.0))
         p_exact0 = cos(x[0])*cos(x[1])*cos(t0)
         p_exact1 = cos(x[0])*cos(x[1])*cos(t1)
         
@@ -49,8 +52,8 @@ for dt in DT:
         dudt1 = diff(u_exact1, t1)
         
         #dudt =  as_vector(( -sin(2*pi*x[1])*cos(2*pi*x[0])*sin(t) , sin(2*pi*x[0])*cos(2*pi*x[1])*sin(t) ))
-        f0 = rho * dudt0 + rho * grad(u_exact0)*(u_exact0 - w_exact0) - div(mu*grad(u_exact0) - p_exact0*Identity(2))
-        f1 = rho * dudt1 + rho * grad(u_exact1)*(u_exact1 - w_exact1) - div(mu*grad(u_exact1) - p_exact1*Identity(2))
+        f0 = rho * dudt0 + rho * grad(u_exact0)*(u_exact0 - w_exact0) - div(2.0*mu*sym(grad(u_exact0)) - p_exact0*Identity(2))
+        f1 = rho * dudt1 + rho * grad(u_exact1)*(u_exact1 - w_exact1) - div(2.0*mu*sym(grad(u_exact1)) - p_exact1*Identity(2))
         #print "dudt = {}".format(dudt(1))
         
         
@@ -73,8 +76,8 @@ for dt in DT:
         u0, p0 = split(up0)
         w0 = Function(W)
         
-        u_exact_int = interpolate(u_exact_e, V)
-        w_exact_int = interpolate(w_exact_e, W)
+        u_exact_int = project(u_exact0, V)
+        w_exact_int = project(w_exact0, W)
         assign(up0.sub(0), u_exact_int) # I want to start with u0 = u_exact_e as initial condition
         assign(w0, w_exact_int)
         
@@ -111,9 +114,9 @@ for dt in DT:
     
         F = Constant(1./dt) * rho * inner(u - u0, v) * dx
         F += rho * inner(grad(u_mid)*(u0 - w0), v) * dx
-        F += mu * inner(grad(u_mid), grad(v)) * dx
-        F -= inner(p*Identity(2), grad(v)) * dx
-        F -= inner(q, div(u)) * dx
+        F += mu * inner(2.0*sym(grad(u_mid)), grad(v)) * dx
+        F -= p * div(v) * dx
+        F -= q * div(u) * dx
         F -= inner(sigma_mid*normal, v) * ds(2)
         F -= inner(f_mid, v) * dx
         
@@ -125,22 +128,10 @@ for dt in DT:
         
         while t_ < (T - 1E-9):
             
-            
+
             t0.assign(t_)    # in this way the constant t should be updated with the value t_
             t1.assign(t_ + dt)
-            
-            # If t is a constant, everything that depends from t is automatically updated
-            u_exact_e.t = t_
-            w_exact_e.t = t_
-            p_exact_e.t = t_
-            # a0, L0 = lhs(F), rhs(F)
-            # 
-            # a1 = inner(grad(w), grad(z)) * dx
-            # L1 = dot(Constant((0.0,0.0)),z)*dx  
-    
-            # plot(u_exact_e, key="uex", title="uex", mesh=mesh)
-            # interactive()
-            
+                        
             A = assemble(a0)
             b = assemble(L0)
             
@@ -171,34 +162,11 @@ for dt in DT:
              # Move the mesh
             ALE.move(mesh, Y)
             mesh.bounding_box_tree().build(mesh)
-            
-            plot(mesh)
-            
-            assign(up0, VP_)
-            # u0, p0 = VP_.split()
-            #plot(u0, title = str(t))
-            #plot(u0, key = "u0", title = "u0", mesh=mesh)
-            #interactive()
-            
+
             t_ += dt
-            
-    
+
+        print "t_ = ", t_
+        print "t1 = ",  float(t1)
         print "||u - uh||_H1 = {0:1.4e}".format(errornorm(u_exact_e, VP_.sub(0), "H1"))
 
-        #Error in L2        
-        # ||u - uh|| = 2.2131e-03
-        # ||u - uh|| = 4.0381e-03
-        # ||u - uh|| = 4.0333e-03
-        # ||u - uh|| = 4.0331e-03
-        # ||u - uh|| = 2.3938e-03
-        # ||u - uh|| = 2.3744e-03
-        # ||u - uh|| = 2.3695e-03
-        # ||u - uh|| = 2.3693e-03
-        # ||u - uh|| = 1.7012e-03
-        # ||u - uh|| = 1.6177e-03
-        # ||u - uh|| = 1.6128e-03
-        # ||u - uh|| = 1.6125e-03
-        # ||u - uh|| = 1.3738e-03
-        # ||u - uh|| = 1.2606e-03
-        # ||u - uh|| = 1.2555e-03
-        # ||u - uh|| = 1.2552e-03
+
